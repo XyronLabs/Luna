d1a898d33f5a341cb63290ab7d77958eddb1b36e
ACharLuk <acharluk@gmail.com>
2017-08-01 20:28:13 +0200

    Breakout 2.0 (?)

--- games/breakout.lua
+++ games/breakout.lua
@@ -1,6 +1,7 @@
 local bricks = {}
 local ball
 local player
+local frames = 0
 
 function newBrick(x, y)
 	return { x = x, y = y, sizex = 40, sizey = 20 }
@@ -16,7 +17,7 @@ function setup()
 	end
 
 	ball = { x = width / 2, y = height - 50, dirx = 1, diry = -1, speed = 5, radius = 20 }
-	player = { x = width / 2 - 50, sizex = 100 }
+	player = { x = width / 2 - 100, sizex = 200 }
 end
 
 function render()
@@ -38,28 +39,33 @@ function render()
 	ball.x = ball.x + ball.dirx * ball.speed
 	ball.y = ball.y + ball.diry * ball.speed
 
-	if ball.x + ball.radius * 2  <= player.x+player.sizex and ball.x >= player.x
-	and ball.y+ball.sizey >= height - 30 then
-		ball.diry = ball.diry * -1
+	if ball.y + ball.radius * 2 > height - 30 then
+		if ball.x + ball.radius >= player.x and ball.x + ball.radius <= player.x + player.sizex then
+			ball.diry = -1
+		end
 	end
 
 
 	clear()
 
+	color(bit32.lshift( bit32.lrotate(0xff0000, math.floor(math.sin(frames) * 0xff0000)), 4) + 0xff)
 	for k, v in pairs(bricks) do
-        if ball.x + ball.radius <= v.x+v.sizex and ball.x + ball.radius >= v.x
-		and (ball.y == v.y+v.sizey or ball.y+ball.radius*2 == v.y) then
-			ball.diry = ball.diry * -1
+		if ball.y <= v.y + v.sizey and ball.y + ball.radius * 2 >= v.y
+			and ball.x <= v.x + v.sizex and ball.x + ball.radius * 2 >= v.x then
+			ball.diry = 1
 			table.remove(bricks, k)
 		end
 		rect(v.x, v.y, v.sizex, v.sizey)
 	end
 
+	color(0xaaaaaaff)
 	circ(ball.x, ball.y, ball.radius)
 	rect(player.x, height - 30, player.sizex, 20)
+
+	frames = frames + 0.000000001
 end
 
 function input()
-	if keys['Left']  then player.x = player.x - 5 end
-	if keys['Right'] then player.x = player.x + 5 end
+	if keys['Left']  then player.x = player.x - 10 end
+	if keys['Right'] then player.x = player.x + 10 end
 end
