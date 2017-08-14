ef8faa7aac1a7370701ce50979376070890410cd
ACharLuk <acharluk@gmail.com>
2017-08-14 13:56:24 +0200

    Finished rainbow demo

--- games/rainbow.lua
+++ games/rainbow.lua
@@ -5,9 +5,6 @@ function setup()
 end
 
 function render()
-    print(currentLine)
-    if currentLine >= height then return end
-
     color(hsva(currentLine / height, 1, 1, 1))
     line(0, currentLine, width, currentLine)
     line(0, currentLine - 1, width, currentLine - 1)
