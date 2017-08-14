240b9985d43193ce84a92eccf76f08f312fcc848
ACharLuk <acharluk@gmail.com>
2017-08-14 13:38:46 +0200

    Brighter colors

--- games/rainbow.lua
+++ games/rainbow.lua
@@ -5,8 +5,12 @@ function setup()
 end
 
 function render()
+    print(currentLine)
+    if currentLine >= height then return end
+
     color(hsva(currentLine / height, 1, 1, 1))
     line(0, currentLine, width, currentLine)
+    line(0, currentLine - 1, width, currentLine - 1)
 
     currentLine = (currentLine % height) + 1
 end
