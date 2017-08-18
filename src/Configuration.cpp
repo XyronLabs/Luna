25ace2316e4d0decde15a172b5fe7e426a4ddde6
ACharLuk <acharluk@gmail.com>
2017-08-18 08:34:11 +0200

    Ignore comments

--- src/Configuration.cpp
+++ src/Configuration.cpp
@@ -1,6 +1,7 @@
 #include "Configuration.hpp"
 
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <string>
 
@@ -8,13 +9,14 @@ Configuration::Configuration(std::string configFilePath) {
     std::ifstream file;
     file.open(configFilePath, std::ifstream::in);
 
-    std::string key, value;
+    std::string line, key;
     
-    while (file.good()) {
-        std::getline(file, key, '=');
-        std::getline(file, value);
-        configData[key] = value;
-        std::cout << key << ":" << configData[key] << std::endl;
+    while (std::getline(file, line)) {
+        if (line[0] == '#') continue;
+        std::istringstream currLine(line);
+
+        if (std::getline(currLine, key, '='))
+            std::getline(currLine, configData[key]);
     }
 
     file.close();
