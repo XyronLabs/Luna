00727083a9fb10d8017d6ee5a3fb215510cba985
ACharLuk <acharluk@gmail.com>
2017-10-31 12:58:41 +0100

    Setup new Windows install

--- build/windows/setup-env.bat
+++ build/windows/setup-env.bat
@@ -1,1 +1,2 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars32.bat"
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars32.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
