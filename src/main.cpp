1b0d879dee325a625b3ece774ea6973c2369d54b
ACharLuk <acharluk@gmail.com>
2017-11-08 12:41:31 +0100

    Change to vector in main

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,8 @@
 
 int main(int argc, char **argv) {
 
-    const char *lua_args = nullptr, *lua_main = nullptr;
+    const char *lua_main = nullptr;
+    std::vector<const char*> lua_args = {nullptr};
 
     for (int i = 0; i < argc; ++i) {
         if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--version") == 0) {
@@ -16,7 +17,7 @@ int main(int argc, char **argv) {
             return EXIT_SUCCESS;
         }
         if (strcmp(argv[i], "-a") == 0 || strcmp(argv[i], "--arguments") == 0) {
-            lua_args = argv[i+1];
+            lua_args.push_back(argv[i+1]);
         }
         if (strcmp(argv[i], "-f") == 0 || strcmp(argv[i], "--file") == 0) {
             lua_main = argv[i+1];
